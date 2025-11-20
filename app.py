0a1b918e5e6c814aa82d2a1ae301c55dd2497df1
CICDProjectUniversity <antoniofracezko@gmail.com>
2025-11-20 15:25:28 +0300

    Добавлен основной файл приложения

---
+++ app.py
@@ -0,0 +1,17 @@
+def add(a, b):
+    """Складывает два числа"""
+    return a + b
+
+def subtract(a, b):
+    """Вычитает второе число из первого"""
+    return a - b
+
+def multiply(a, b):
+    """Умножает два числа"""
+    return a * b
+
+if __name__ == "__main__":
+    print("Калькулятор работает!")
+    print(f"2 + 3 = {add(2, 3)}")
+    print(f"5 - 2 = {subtract(5, 2)}")
+    print(f"4 * 3 = {multiply(4, 3)}")
